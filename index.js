f5e299728afdfb3f0a0f932af97a9cfbb4ce3442
SRochaGabriel <gabrielrocha2004.gr@gmail.com>
2025-11-30 12:00:45 -0300

    Projetos em swiper

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const navbarLinks = document.querySelector('.navbar-links');
 const navLinks = document.querySelectorAll('.navbar-links a');
 
 // Acessando a div 'projetos_display'
-const projetosDisplay = document.querySelector('.projetos_display');
+const projetosDisplay = document.querySelector('.swiper-wrapper');
 
 // Ao botão ser clicado, a div 'navbar-links' e o próprio botão recebem ou perdem a classe 'aberto'
 btn_menu_toggle.addEventListener('click', () => {
@@ -24,31 +24,56 @@ navLinks.forEach(link => {
     });
 });
 
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 20,
+    loop: true,
+    grabCursor: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        1024: {
+            slidesPerView: 3,
+        },
+        640: {
+            slidesPerView: 2,
+        },
+        425: {
+            slidesPerView: 1,
+        }
+    }
+});
+
 // Buscando os itens de projetos.json e gerando os cards que exibem os projetos
 fetch('projetos.json')
 .then(res => res.json())
 .then(projects => {
-    console.log(projects)
-
     projects.forEach(project => {
         console.log(project)
         projetosDisplay.innerHTML += `
-            <div class="card_projeto" id="${project.id}">
-                <img class="projeto_cover" src="${project.image}" alt="Imagem do projeto">
-                <div class="card_info">
-                    <div class="card_projeto_header">
-                        <h3>${project.name}</h3>
-                        <p>${project.desc}</p>
-                    </div>
-                    <div class="card_projeto_body"></div>
-                    <div class="card_projeto_footer">
-                        <a href="${project.deploy}">
-                            <img class="link_projeto_icon" src="./imagens/link_logo.png" alt="Link deploy">
-                            <p>Acesse o site</p>
-                        </a>
-                        <a href="${project.rep}" target="_blank">
-                            <img class="link_projeto_icon" src="./imagens/github_logo_white.png" alt="Link Github">
-                        </a>
+            <div class="swiper-slide">
+                <div class="card_projeto" id="${project.id}">
+                    <img class="projeto_cover" src="${project.image}" alt="Imagem do projeto">
+                    <div class="card_info">
+                        <div class="card_projeto_header">
+                            <h3>${project.name}</h3>
+                            <p>${project.desc}</p>
+                        </div>
+                        <div class="card_projeto_body"></div>
+                        <div class="card_projeto_footer">
+                            <a href="${project.deploy}">
+                                <img class="link_projeto_icon" src="./imagens/link_logo.png" alt="Link deploy">
+                                <p>Acesse o site</p>
+                            </a>
+                            <a href="${project.rep}" target="_blank">
+                                <img class="link_projeto_icon" src="./imagens/github_logo_white.png" alt="Link Github">
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
